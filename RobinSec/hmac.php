1303eabe74d484dd89ece8dc44f19bff29437668
GTboy68 <robin.callay@student.ap.be>
2015-06-18 12:42:41 +0200

    Delete Security taak

--- RobinSec/hmac.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo 'Dit is de teststring ge&euml;ncrypteerd:   ';
-echo hash_hmac('ripemd160', 'Dit is de teststring ge&euml;ncrypteerd', 'secret');
-?>
-
-<a href="http://pastebin.com/aNMVKMiJ">hmac code</a>
